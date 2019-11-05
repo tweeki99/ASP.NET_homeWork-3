d6f8eb32cb0b0bdb578d597e3c87ba87ecac9838
danil.shipilov.99 <danil.shipilov.99@gmail.com>
2019-11-05 18:21:30 +0600

    hw3

---
+++ HomeWork3/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWork3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(int id)
+        {
+            return File($@"C:\Users\шипиловд\source\repos\HomeWork3\HomeWork3\{id}.mp3", "audio/mpeg", $"result{id}.mp3");
+        }
+
+        public ActionResult All()
+        {
+            return File(@"C:\Users\шипиловд\source\repos\HomeWork3\HomeWork3\All.rar", "application/zip", $"result.zip");
+        }
+    }
+}
